31d6a5236aa19834eb99ea3467426e93e618247c
Dima Iliov <dmitriy.iliyov@gmail.com>
2023-12-19 11:05:27 +0200

    del

--- SF_lab_2/src/main/resources/static/js/doctors_logic.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.getValue').addEventListener('click', ()=>{
-    let data = document.querySelector('.doctors').value;
-    if (data === dc1) {
-        alert("jihhgjhijok")
-    } else {
-        alert("(")
-    }
-})
