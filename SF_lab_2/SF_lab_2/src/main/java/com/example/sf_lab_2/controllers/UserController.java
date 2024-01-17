78630a6e3c863c5190b785f342fe450af1f36e39
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-01-18 01:06:44 +0200

    user controller

--- SF_lab_2/SF_lab_2/src/main/java/com/example/sf_lab_2/controllers/UserController.java
+++ SF_lab_2/SF_lab_2/src/main/java/com/example/sf_lab_2/controllers/UserController.java
@@ -1,2 +1,21 @@
-package com.example.sf_lab_2.controllers;public class UserController {
+package com.example.sf_lab_2.controllers;
+
+import com.example.sf_lab_2.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user-interface")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String userHomePage(){
+        return "user_home_page";
+    }
 }
