909fcfc014bb051dd206a2d0ef487f2a791063b9
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-01-18 01:33:25 +0200

    services

--- SF_lab_2/SF_lab_2/src/main/java/com/example/sf_lab_2/service/AdministratorService.java
+++ SF_lab_2/SF_lab_2/src/main/java/com/example/sf_lab_2/service/AdministratorService.java
@@ -7,24 +7,19 @@ import java.util.Date;
 
 @Service
 public class AdministratorService {
-    private PolyclinicRepository timeTable;
+    private PolyclinicRepository repository;
 
     public AdministratorService() {
 
     }
 
     private void addAppointment(String doctorName, Date time, Long userID) {
-        if(PolyclinicRepository.checkDoctorExistence(doctorName)){
-
-        }
-        this.timeTable.getDoctor(doctorName);
     }
 
     private void deleteAppointment(String doctorName, Date time, Long userID) {
-        this.timeTable.getDoctor(doctorName);
+
     }
 
     private void updateAppointment(String doctorName, Date time, Long userID) {
-        this.timeTable.getDoctor(doctorName);
     }
 }
--- SF_lab_2/SF_lab_2/src/main/java/com/example/sf_lab_2/service/UserService.java
+++ SF_lab_2/SF_lab_2/src/main/java/com/example/sf_lab_2/service/UserService.java
@@ -1,2 +1,25 @@
-package com.example.sf_lab_2.service;public class UserService {
+package com.example.sf_lab_2.service;
+
+import com.example.sf_lab_2.repository.PolyclinicRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class UserService {
+    private PolyclinicRepository repository;
+
+    public UserService() {
+
+    }
+
+    private void addAppointment(String doctorName, Date time, Long userID) {
+    }
+
+    private void deleteAppointment(String doctorName, Date time, Long userID) {
+
+    }
+
+    private void updateAppointment(String doctorName, Date time, Long userID) {
+    }
 }
