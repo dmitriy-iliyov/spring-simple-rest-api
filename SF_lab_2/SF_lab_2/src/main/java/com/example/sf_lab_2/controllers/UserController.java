ee141f982a658abbdba74a58c75e62b846142a7e
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-01-20 05:47:07 +0200

    UserController

--- SF_lab_2/SF_lab_2/src/main/java/com/example/sf_lab_2/controllers/UserController.java
+++ SF_lab_2/SF_lab_2/src/main/java/com/example/sf_lab_2/controllers/UserController.java
@@ -1,13 +1,15 @@
 package com.example.sf_lab_2.controllers;
 
+import com.example.sf_lab_2.models.Doctor;
 import com.example.sf_lab_2.models.TimeTable;
+import com.example.sf_lab_2.models.User;
 import com.example.sf_lab_2.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/user-interface")
+@RequestMapping("/user")
 public class UserController {
     private UserService userService;
 
@@ -24,19 +26,56 @@ public class UserController {
 //    public String allDoctors(){
 //
 //    }
+    @GetMapping("/add")
+    public String userForm(Model model){
+        model.addAttribute("user", new User());
+        return "user_register_form";
+    }
+
+    @PostMapping("/add")
+    public String userSubmit(@ModelAttribute Doctor doctor, Model model){
+        model.addAttribute("doctor", doctor);
+        System.out.println(doctor);
+        //
+        return "user_register_form";
+    }
+
+    @GetMapping("/get/{name}")
+    public String getUser(@PathVariable("name") String doctor, Model model){
+        return "current_user";
+    }
+
+    @GetMapping("/get")
+    public String getUser(){
+        return "doctors";
+    }
+
+    @PostMapping("/change")
+    public String userSubmitChanges(@ModelAttribute Doctor doctor, Model model){
+        model.addAttribute("doctor", doctor);
+        System.out.println(doctor);
+        //
+        return "user_register_form";
+    }
+
+    @GetMapping("/change")
+    public String userFormForChange(Model model){
+        model.addAttribute("doctor", new Doctor());
+        return "user_register_form";
+    }
 
     @GetMapping("/get-doctor/{name}")
     public String getDoctor(@PathVariable("name") String doctor, Model model){
         return "current_doctor";
     }
 
-    @GetMapping("/doctor-time-table")
+    @GetMapping("/get-doctor/time-table")
     public String appointmentsForm(Model model) {
         model.addAttribute("timeTable", new TimeTable());
         return "doctors_time_table";
     }
 
-    @PostMapping("/doctor-time-table")
+    @PostMapping("/get-doctor/time-table")
     public String appointmentsSubmit(@ModelAttribute TimeTable timeTable, Model model){
         model.addAttribute("timeTable", timeTable);
         System.out.println(timeTable);
